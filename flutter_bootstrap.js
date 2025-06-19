0deae63bf3ec994ce7bbe6bcf59637fdbb2a6ee9
duckhiem2003 <duckhiem2003@gmail.com>
2025-06-19 17:50:31 +0700

    update navigate to landing page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3804725059"
+    serviceWorkerVersion: "1737055281"
   }
 });
